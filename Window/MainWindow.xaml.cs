5ac30037a6e8be51c3e45df43e628c80df1b78f7
Klop233 <3337913379@qq.com>
2023-01-31 12:01:05 +0800

    改为反射添加Resource

--- Window/MainWindow.xaml.cs
+++ Window/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,39 +24,48 @@ namespace WaifuViewer {
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
     public partial class MainWindow : Window {
-        private static String[] waifus = { "Mashiro", "Asuka" };
+        private static string[] waifus = { "Mashiro", "Asuka" };
         private static Hashtable photos = new Hashtable();
         private static int num = 0;
         private static int pic = 0;
-        private static string title = "Waifu 查看器 [WPF C# Demo By Klop233] [Image from Aokana] ";
+        private static string defaultTitle = "Waifu 查看器 [WPF C# Demo By Klop233] [Image from Aokana] ";
 
+       
 
         public MainWindow()
         {
-            ImageSource[] Mashiro =
-            {
-                toSource(Properties.Resources.Mashiro1),
-                toSource(Properties.Resources.Mashiro2),
-                toSource(Properties.Resources.Mashiro3),
-                toSource(Properties.Resources.Mashiro4)
-            };
+            // Refelction class
+            string resource = "WaifuViewer.Properties.Resources";
+            Type type = Type.GetType(resource);
+            var Asuka = new List<ImageSource>();
+            var Mashiro = new List<ImageSource>();
+            // Image count of a character
+            int n = 0;
 
-            ImageSource[] Asuka =
+            foreach (PropertyInfo p in type.GetProperties())
             {
-                toSource(Properties.Resources.Asuka1),
-                toSource(Properties.Resources.Asuka2),
-                toSource(Properties.Resources.Asuka3),
-                toSource(Properties.Resources.Asuka4),
-                toSource(Properties.Resources.Asuka5)
-            };
+                if (p.Name.StartsWith("Asuka")) {
+                    Asuka.Add(ToSource(p.GetValue(type) as Bitmap));
+                }
+                n++;
+            }
 
-            photos.Add("Mashiro", Mashiro);
+            n = 0;
+
+            foreach (PropertyInfo p in type.GetProperties())
+            {
+                if (p.Name.StartsWith("Mashiro")) {
+                    Mashiro.Add(ToSource(p.GetValue(type) as Bitmap));
+                }
+            }
+
             photos.Add("Asuka", Asuka);
+            photos.Add("Mashiro", Mashiro);
 
             InitializeComponent();
         }
 
-        private ImageSource toSource(Bitmap img)
+        private ImageSource ToSource(Bitmap img)
         {
             IntPtr ptr = img.GetHbitmap();
             var imageSource = Imaging.CreateBitmapSourceFromHBitmap(ptr, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
@@ -74,23 +84,24 @@ namespace WaifuViewer {
                 num++;
             }
             pic = 0;
-            PhotoBox.Source = ((ImageSource[]) photos[waifus[num]])[pic];
-            Title = title + string.Format("[Current:{0} Pic: {1}]", waifus[num], pic+1);
+            PhotoBox.Source = (photos[waifus[num]] as List<ImageSource>)[pic];
+            Title = defaultTitle + string.Format("[Current:{0} Pic: {1}]", waifus[num], pic+1);
         }
 
         private void OnSwitchPhoto(object sender, RoutedEventArgs e)
         {
             string waifu = waifus[num];
-            ImageSource[] imgs = (ImageSource[]) photos[waifu];
-            if (pic+1 >= imgs.Length)
+            
+            var imgs = photos[waifu] as List<ImageSource>;
+            if (pic+1 >= imgs.Count)
             {
                 pic = 0;
                 PhotoBox.Source = imgs[pic];
-                Title = title + string.Format("[Current:{0} Pic: {1}]", waifus[num], pic + 1);
+                Title = defaultTitle + string.Format("[Current:{0} Pic: {1}]", waifus[num], pic + 1);
                 return;
             }
             pic++;
-            Title = title + string.Format("[Current:{0} Pic: {1}]", waifus[num], pic + 1);
+            Title = defaultTitle + string.Format("[Current:{0} Pic: {1}]", waifus[num], pic + 1);
             PhotoBox.Source = imgs[pic];
             
         }
